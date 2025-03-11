50f05ef0e4fc9ad62d4fdf964579656266ace044
Moon2322 <aldito201401@gmail.com>
2025-03-10 19:40:11 -0600

    Recargar-page-3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Asegúrate de que sea '/'
-  build: {
-    outDir: 'dist'
-  }
-});
-
+})
